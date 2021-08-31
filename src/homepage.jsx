f55ee3246bbf3aebd2b9caa09a1763ccf92b5707
Tei Mensah <twomensah@gmail.com>
2021-08-31 13:47:49 +0100

    pollPush

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -3,7 +3,14 @@ import "./homepage.css";
 
 class homepage extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="homepage_container">
+        <h1>Welcome,User</h1>
+        <button className="poll_btn">Setup Poll</button>
+        <button className="poll_btn">View Poll</button>
+        <button className="poll_btn">Design Voter</button>
+      </div>
+    );
   }
 }
 
